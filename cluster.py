8955ba052fe785e5a9e3085ddd3c59b7c01b921c
Butler <jbutler72@gatech.edu>
2017-04-15 03:56:29 -0400

    Adding cluster.py

---
+++ cluster.py
@@ -0,0 +1,40 @@
+import cv2
+import os
+import numpy as np
+from scipy.misc import imread, imresize
+from kmeans import TFKMeansCluster
+
+K = 4
+DATA_DIR = "../../data/ImagesTrain_Sorted/Outdoors"
+
+
+def main():
+
+	vectors = getData()
+
+	print("Entering K-Means")
+	TFKMeansCluster(vectors, K)
+
+
+
+
+# Get list of vectors
+def getData():
+	vectors = []
+
+	for jpg in os.listdir(DATA_DIR):
+		# print("JPG: " + jpg)
+		image = imread(DATA_DIR + "/" + jpg)
+
+		print("img shape: " + str(image.shape))
+
+		image = image.astype(np.float64)
+		#cv2.imshow("a", image)
+		#cv2.waitKey()
+		vectors.append(image);
+
+	print(vectors)
+	return vectors
+
+if __name__ == "__main__":
+	main()
